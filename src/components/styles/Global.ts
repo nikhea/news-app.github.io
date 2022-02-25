4070023c47db734d6540ff4fb00c795fb551103c
nikhea <imonikheaugbodaga@gmail.com>
2022-02-25 22:42:01 +0000

    added global-styles and themeProvider

---
+++ src/components/styles/Global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle =createGlobalStyle`
+   *{
+       margin: 0;
+       padding: 0;
+       outline:0;
+       box-sizing:border-box;
+       font-family: 'Open Sans', sans-serif; 
+   }
+   img{
+       width: 100%;
+       max-width: 100%;
+   }
+   `;
+
+export const theme = {
+
+   }
